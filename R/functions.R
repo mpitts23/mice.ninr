b262e10ab47d9fc767f8859ada46c27f2b36065d
mpitts23 <mpitts23@amherst.edu>
2023-04-20 22:08:49 -0400

    adding mice dependency and pooling functions

--- R/functions.R
+++ R/functions.R
@@ -1,22 +1,6 @@
 
 
-#' Title
-#'
-#' @param data
-#' @param m
-#' @param maxit
-#' @param printFlag
-#' @param control_data
-#' @param intervention_data
-#' @param control_delta
-#' @param intervention_delta
-#' @param additive
-#' @param dependent_var
-#'
-#' @return
-#' @export
-#'
-#' @examples
+
 mice_ninr <- function(data,
                       m = 5,
                       maxit = 5,
@@ -24,10 +8,9 @@ mice_ninr <- function(data,
                       control_data,
                       intervention_data,
                       control_delta = c(0, 0, 0, 0),
-                      intervention_delta = c(0, -50, 100, -1000),
+                      intervention_delta = c(0,-50, 100,-1000),
                       additive = TRUE,
-                      dependent_var){
-
+                      dependent_var) {
   imp <- mice(data,
               maxit = 0,
               printFlag = FALSE)
@@ -44,8 +27,10 @@ mice_ninr <- function(data,
   post <- rep(list(imp$post), length(delta_G0))
   dependent_var <- as.list(rep(dependent_var, length(delta_G0)))
 
-  parameters_G0 <- list(delta_G0, df_G0, additive, post, dependent_var)
-  parameters_G1 <- list(delta_G1, df_G1, additive, post, dependent_var)
+  parameters_G0 <-
+    list(delta_G0, df_G0, additive, post, dependent_var)
+  parameters_G1 <-
+    list(delta_G1, df_G1, additive, post, dependent_var)
 
   list_mids_G0 <- purrr::pmap(parameters_G0, postFunction)
   list_mids_G1 <- purrr::pmap(parameters_G1, postFunction)
@@ -55,47 +40,26 @@ mice_ninr <- function(data,
   list_mids_G1_G0 <- purrr::pmap(parameters_mids, sens_combined)
 }
 
-#' Title
-#'
-#' @param i
-#' @param data
-#' @param additive
-#' @param post
-#' @param dependent_var
-#'
-#' @return
-#' @export
-#'
-#' @examples
 postFunction <- function(i, data, additive, post, dependent_var) {
-
-  if(additive == TRUE){
+  if (additive == TRUE) {
     operation = "+"
   }
-  else if(additive == FALSE){
+  else if (additive == FALSE) {
     operation = "*"
   }
   d <- i
   cmd <- paste("imp[[j]][,i] <- imp[[j]][,i]", operation, d)
   post[paste(dependent_var)] <- cmd
-  imp <- mice(data,
-              post = post,
-              maxit = 1,
-              m = 200,
-              print = FALSE
+  imp <- mice(
+    data,
+    post = post,
+    maxit = 1,
+    m = 200,
+    print = FALSE
   )
   return(imp)
 }
 
-#' Title
-#'
-#' @param mids1
-#' @param mids2
-#'
-#' @return
-#' @export
-#'
-#' @examples
 sens_combined <- function(mids1, mids2) {
   data_G1 <- complete(mids1, action = 'long', include = TRUE)
   data_G0 <- complete(mids2, action = 'long', include = TRUE) %>%
@@ -109,3 +73,18 @@ sens_combined <- function(mids1, mids2) {
   return(mids_G1_G0)
 }
 
+pool_function <- function(mids, delta_0, delta_1) {
+  df <- as.data.frame(summary(mice::pool(with(
+    data = mids,
+    expr = lm(typical_occFUP ~ auditCscore + intervention_group)
+  )))) %>%
+    mutate(delta_0 = round(delta_0, 2),
+           delta_1 = round(delta_1, 2))
+  return(df)
+}
+
+combine_pool <- function(list_mids, delta_G0, delta_G1){
+  parameters <- list(list_mids, delta_G0, delta_G1)
+  fit <- purrr::pmap_dfr(parameters, pool_function)
+  return(fit)
+}
